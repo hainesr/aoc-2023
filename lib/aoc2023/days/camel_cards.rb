0f942459e2090d63df8d0256ae970d983b22d4f9
Robert Haines <rhaines@manchester.ac.uk>
2023-12-07 23:09:22 +0000

    Minor adjustments for readability for day 7.
    And factor out the `calculate_winnings` formula.

--- lib/aoc2023/days/camel_cards.rb
+++ lib/aoc2023/days/camel_cards.rb
@@ -17,36 +17,40 @@ module AOC2023
     end
 
     def part1
-      sorted = @hands.sort_by { |_, h, _| [sort_helper(h), h] }
-
-      sorted.each.with_index(1).sum { |(_, _, b), i| b * i }
+      calculate_winnings(
+        @hands.sort_by { |_, hand, _| [sort_helper(hand), hand] }
+      )
     end
 
     def part2(joker = CARD_MAP['J'])
-      sorted = @hands.sort_by do |_, h, _|
-        hand_no_jokers = h.reject { |c| c == joker }
-        num_jokers = h.count(joker)
-        hand_rescored_jokers = h.map { |c| c == joker ? -1 : c }
+      sorted = @hands.sort_by do |_, hand, _|
+        hand_no_jokers = hand.reject { |c| c == joker }
+        num_jokers = hand.count(joker)
+        hand_rescored_jokers = hand.map { |c| c == joker ? -1 : c }
 
         [sort_helper(hand_no_jokers, num_jokers), hand_rescored_jokers]
       end
 
-      sorted.each.with_index(1).sum { |(_, _, b), i| b * i }
+      calculate_winnings(sorted)
+    end
+
+    def calculate_winnings(ranked_hands)
+      ranked_hands.each.with_index(1).sum { |(_, _, bid), i| bid * i }
     end
 
     def sort_helper(hand, num_jokers = 0)
       card_groups = hand.tally
-      card_group_frequencies = card_groups.values.sort
+      card_group_frequencies = card_groups.values.sort.reverse
 
       # Jokers should be used as duplicates of the most frequent card,
       # but handle the case of a hand of all jokers first.
       if num_jokers == 5
         card_group_frequencies = [5]
       else
-        card_group_frequencies[-1] += num_jokers
+        card_group_frequencies[0] += num_jokers
       end
 
-      card_group_frequencies.reverse
+      card_group_frequencies
     end
 
     # Returns a list of ['hand', [values], bid]
