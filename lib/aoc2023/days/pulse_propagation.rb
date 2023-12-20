18724c78e7a7fdca4ae637d7cfb3c82b35476ce0
Robert Haines <rhaines@manchester.ac.uk>
2023-12-20 17:21:09 +0000

    Solution for day 20, part 2.

--- lib/aoc2023/days/pulse_propagation.rb
+++ lib/aoc2023/days/pulse_propagation.rb
@@ -31,6 +31,34 @@ module AOC2023
       pulse_counts[true] * pulse_counts[false]
     end
 
+    def part2
+      # Need the source to 'rx' to send a low pulse. The source of 'rx' is a
+      # conjunction (&) so we need *its* sources to all have high pulses.
+      # Find the periods of all those sources going high, and we can do LCM
+      # for the answer.
+      rx_source = @sources['rx'].first # Hopefully there's only one :-s
+      high_times = @sources[rx_source].to_h { |s| [s, []] }
+
+      # We just did 1000 pushes, so start at push 1001.
+      1001.step do |t|
+        queue = [['broadcaster', 'button', false]]
+
+        until queue.empty?
+          queue = queue.flat_map do |to, from, pulse|
+            if pulse && to == rx_source
+              high_times[from] << t
+
+              if high_times.values.all? { |v| v.size >= 3 }
+                return periods(high_times).reduce(1) { |acc, period| acc.lcm(period) }
+              end
+            end
+
+            @components[to][pulse, from]
+          end
+        end
+      end
+    end
+
     def read_config(input)
       destinations = input.each_line(chomp: true).to_h do |line|
         name, outs = line.split(' -> ')
@@ -44,6 +72,14 @@ module AOC2023
       sources.default_proc = nil # No longer want to return [] by default.
 
       [sources, build_components(sources, destinations)]
+    end
+
+    private
+
+    def periods(times)
+      times.values.map do |ts|
+        ts.each.with_index(1).all? { |t, i| t == ts[0] * i } ? ts[0] : nil
+      end
     end
 
     def build_components(sources, destinations) # rubocop:disable Metrics/CyclomaticComplexity
